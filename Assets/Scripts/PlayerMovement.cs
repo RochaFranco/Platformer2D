36809e3e15a9ded072d3c963c709a1107eabdb89
GPR <rochafrancoagustin@hotmail.com>
2022-07-14 21:03:23 -0300

    se creo el proyecto de unity y se agrego movimiento al jugador

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float velocity;
+    public float jumpForce;
+    public bool canJump;
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        transform.position = transform.position + new Vector3 (Input.GetAxis("Horizontal") * velocity * Time.deltaTime, 0, 0);
+
+        if (Input.GetKeyDown(KeyCode.Space) && canJump == true)
+        {
+            rb.AddForce(new Vector2(0,jumpForce),ForceMode2D.Impulse);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Floor")
+        {
+            canJump = true;
+        }
+        else
+        {
+            canJump = false;
+        }
+    }
+}
